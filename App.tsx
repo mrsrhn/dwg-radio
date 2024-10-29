92f295c6da1e33f4a7dd52362e08c069b3ca2031
Marius Rhein <inbox@mariusrhein.de>
2024-10-29 19:44:00 +0100

    fix: move queryclient out of react component

--- App.tsx
+++ App.tsx
@@ -4,15 +4,15 @@ import Layout from './src/components/layout';
 import StoreProvider from './src/providers/storeProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export default function App() {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        gcTime: 0,
-      },
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 30 * 1000,
     },
-  });
+  },
+});
 
+export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <StoreProvider>
